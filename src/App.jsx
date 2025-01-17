ec3228a202bb0e5d43bac4d56082d9028306b256
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2025-01-17 15:34:52 +0500

    6-oy 10-dara

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Pagination } from '@mui/material';
+import axios from 'axios';
+
+function App() {
+  const [comments, setComments] = useState([]);
+  const [current, setCurrent] = useState(1);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/comments?_page=${current}&_limit=2`)
+      .then(response => {
+        if (response.status === 200) {
+          setComments(response.data);
+        }
+      });
+  }, [current]);
+
+  function handlePagination(event, position) {
+    setCurrent(position);
+  }
+
+  return (
+    <div>
+      <div className="container mx-auto mt-10">
+        {comments.length > 0 && comments.map((item, index) => {
+          return (
+            <div key={index} className="p-3 border rounded-md shadow-md">
+              <h3>{item.name}</h3>
+              <p>{item.body}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <Pagination onChange={handlePagination} count={10} variant="outlined" color="primary" />
+    </div>
+  );
+}
+
+export default App;
