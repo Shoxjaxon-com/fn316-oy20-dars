795768e2b6ec65cd60818f376caa681bd92b3e2a
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2025-01-17 21:53:24 +0500

    vazifa

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import axios from 'axios';
 
 function App() {
   const [comments, setComments] = useState([]);
-  const [current, setCurrent] = useState(1);
+  const [current, setCurrent] = useState(() => {
+    return parseInt(localStorage.getItem('currentPage')) || 1;
+  });
 
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/comments?_page=${current}&_limit=2`)
-      .then(response => {
+    // API chaqiruv
+    axios
+      .get(`https://jsonplaceholder.typicode.com/comments?_page=${current}&_limit=2`)
+      .then((response) => {
         if (response.status === 200) {
           setComments(response.data);
         }
       });
+
+    localStorage.setItem('currentPage', current);
   }, [current]);
 
   function handlePagination(event, position) {
@@ -22,17 +28,22 @@ function App() {
   return (
     <div>
       <div className="container mx-auto mt-10">
-        {comments.length > 0 && comments.map((item, index) => {
-          return (
+        {comments.length > 0 &&
+          comments.map((item, index) => (
             <div key={index} className="p-3 border rounded-md shadow-md">
               <h3>{item.name}</h3>
               <p>{item.body}</p>
             </div>
-          );
-        })}
+          ))}
       </div>
 
-      <Pagination onChange={handlePagination} count={10} variant="outlined" color="primary" />
+      <Pagination
+        onChange={handlePagination}
+        page={current} 
+        count={10}
+        variant="outlined"
+        color="primary"
+      />
     </div>
   );
 }
